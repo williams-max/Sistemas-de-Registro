605dc31cae0f7b2c9e06518dcd4cc35a54fe425a
williams-max <almanzaisrael75@gmail.com>
2021-01-02 22:33:55 -0700

    verificacion de solapmiento de fechas

--- resources/views/reporte/index.blade.php
+++ resources/views/reporte/index.blade.php
@@ -163,7 +163,7 @@ use Illuminate\Support\Facades\DB;
 <script type="text/javascript">
 
  
-//dsfsdfsdfsdasdasd
+//dsfsdfsdfsdasdasdsdfsdfsdfsdfsdf
     formulario.addEventListener('submit', (e) => {
     
          console.log("eventos de sumbits");
